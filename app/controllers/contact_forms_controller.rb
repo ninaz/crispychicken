4c7bcda2b1cd837cbd90f2e73c5a581a728a6617
Asitha de Silva <asithade@gmail.com>
2013-12-19 03:06:26 +0000

    Email Uniqueness - #60

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -1,21 +1,21 @@
 class ContactFormsController < ApplicationController
-skip_before_filter :require_signin
-    def new
-      @contact_form = ContactForm.new
-    end
+  skip_before_filter :require_signin
+  def new
+    @contact_form = ContactForm.new
+  end
 
-    def thank_you
-      begin
-        @contact_form = ContactForm.new(params[:contact_form])
-        @contact_form.request = request
-        if @contact_form.valid?
-          @contact_form.deliver
-          render :thank_you
-        else
-          render :new
-        end
-      rescue ScriptError
-        flash[:error] = 'Sorry, this message appears to be spam and was not delivered.'
+  def thank_you
+    begin
+      @contact_form = ContactForm.new(params[:contact_form])
+      @contact_form.request = request
+      if @contact_form.valid?
+        @contact_form.deliver
+        redirect_to root_url, :notice => "Thanks! We'll be in touch as soon as we can!"
+      else
+        render :new
       end
+    rescue ScriptError
+      flash[:error] = 'Sorry, this message appears to be spam and was not delivered.'
     end
   end
+end
